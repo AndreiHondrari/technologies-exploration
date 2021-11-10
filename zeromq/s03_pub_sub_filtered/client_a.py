c448cca32131332f8fdd173f4a7b88efe4ef9a88
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2021-11-10 10:52:09 +0100

    Zmq pub-sub filtered

---
+++ zeromq/s03_pub_sub_filtered/client_a.py
@@ -0,0 +1,20 @@
+import random
+
+import zmq
+
+CATEGORY = "category_1"
+
+
+if __name__ == '__main__':
+    UID = random.randint(1000, 10_000)
+    print(f"Starting client [{UID}]...")
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://localhost:5556")
+
+    socket.setsockopt_string(zmq.SUBSCRIBE, CATEGORY)
+
+    print("Listening for news")
+    while True:
+        message = socket.recv_string()
+        print(f"[{UID}] news: {message}")
