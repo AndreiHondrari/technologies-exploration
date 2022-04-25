0907243a0229754c87166c0137a9cc210120b7bc
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2022-04-25 14:28:15 +0200

    Add sqlalchemy table reflection sample

---
+++ python-orm-databases/sqlalchemy/s07_table_reflection.py
@@ -0,0 +1,41 @@
+from pprint import pprint
+from functools import partial
+
+from sqlalchemy import (
+    create_engine, MetaData, Table,
+    text,
+)
+
+hprint = partial(print, " \n#")
+
+
+def main() -> None:
+    # create engine
+    engine = create_engine(
+        "sqlite+pysqlite:///:memory:",
+        echo=True,
+    )
+
+    # NOTICE absence of a primary key
+    with engine.connect() as conn:
+        conn.execute(text(
+            """
+            CREATE TABLE item (
+                val INTEGER,
+                title VARCHAR(30)
+            )
+            """
+        ))
+
+    # init metadata
+    metadata = MetaData()
+
+    # declara table
+    item_table = Table("item", metadata, autoload_with=engine)
+
+    hprint("Item table reflected")
+    pprint(item_table)
+
+
+if __name__ == "__main__":
+    main()
