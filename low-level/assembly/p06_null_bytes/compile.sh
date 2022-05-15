5b571eedc3fd0e5d3aa8d2e86f772d1553c9860e
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2022-05-15 11:53:31 +0200

    Add and adapt compile and run scripts for multiple assembly samples

--- low-level/assembly/p06_null_bytes/compile.sh
+++ low-level/assembly/p06_null_bytes/compile.sh
@@ -1,9 +1,11 @@
-echo "*** COMPILE ASM PROGRAM ***"
-echo "Generating obj file ..."
-nasm -g -F dwarf -f elf src/program.asm -o dist/program.o
+echo "*** COMPILE ASM PROGRAMS ***"
+echo "Generating obj files ..."
+nasm -g -F dwarf -f elf src/p01_without_nullbytes.asm -o dist/p01.o
+nasm -g -F dwarf -f elf src/p02_with_nullbytes.asm -o dist/p02.o
 
-echo "Linking into executable ..."
-ld -m elf_i386 dist/program.o -o dist/program
+echo "Linking into executables ..."
+ld -m elf_i386 dist/p01.o -o dist/p01
+ld -m elf_i386 dist/p02.o -o dist/p02
 
 echo "DONE"
 echo ""
---
+++ low-level/assembly/p06_null_bytes/run.sh
@@ -0,0 +1,7 @@
+echo "**WITHOUT NULLBYTES**"
+./dist/p01
+echo ""
+
+echo "**WITH NULLBYTES**"
+./dist/p02
+echo ""
