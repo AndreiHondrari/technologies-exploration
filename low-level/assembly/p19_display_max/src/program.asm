3592ee6a450c5126c94a7c38fceb998f23be5b98
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2022-05-16 21:59:30 +0200

    Add assembly p19 sample to display max for 0xFFFFFFFF

---
+++ low-level/assembly/p19_display_max/src/program.asm
@@ -0,0 +1,33 @@
+
+%include 'src/functions.asm'
+
+SECTION .data
+startmsg db '** Hello Char! **', 0x00
+endmsg db '** END **', 0x00
+
+SECTION .bss
+
+SECTION .text
+global _start
+
+printstart:
+    mov eax, startmsg
+    call sprintLF
+    call printLF
+    ret
+
+printend:
+    call printLF
+    mov eax, endmsg
+    call sprintLF
+    ret
+
+_start:
+    call printstart
+
+    ; generate array of digits from a given integer
+    mov eax, 0xFFFFFFFF ; 4 294 967 295 (10 bytes)
+    call print_integer
+
+    call printend
+    call quit
