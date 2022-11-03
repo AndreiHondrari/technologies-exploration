e9d5f118b8f807d868fed7489814bf1225857905
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2022-11-03 09:14:41 +0100

    Make the rust prints examples better

--- programming-languages/rust/01_fundamentals/02_01_prints.rs
+++ programming-languages/rust/01_fundamentals/02_01_prints.rs
@@ -2,17 +2,42 @@
 Various ways to print.
 */
 
+// declare some static strings
+const MESSAGE_A: &'static str = "aaa";
+const MESSAGE_B: &'static str = "bbb";
+const MESSAGE_C: &'static str = "ccc";
+const MESSAGE_D: &'static str = "ddd";
+const MESSAGE_E: &'static str = "eee";
+const MESSAGE_F: &'static str = "fff";
+
 fn main() {
-    print!("aaa");
+    // print directly without newline
+    print!("000__");
 
-    const Y: &'static str = "bbb";
-    print!("{}", Y);
+    // print directly with new line
+    println!("111");
 
-    let z: String = String::from("ccc");
+    // print a literal as a positional parameter
+    println!("{}", "222");
 
-    print!("{}", z);
-    print!(" ... {z}\n");
+    // print a static string as a positional parameter
+    println!("{}", MESSAGE_A);
 
-    println!("{}, {}", Y, z);
-    println!("{}, {}", "ddd", "eee");
+    // print static string in the inline format
+    println!("{MESSAGE_B}");
+
+    // print a composed formatted message
+    println!("this {} that", "333");
+
+    // print multiple literals as positional parameters
+    println!("{}, {}", "444", "555");
+
+    // print multiple static strings as positional parameters
+    println!("{}, {}", MESSAGE_A, MESSAGE_B);
+
+    // print multiple static strings in the inline format
+    println!("{MESSAGE_C}, {MESSAGE_D}");
+
+    // print multiple static strings as positional and inline
+    println!("{}, {MESSAGE_F}", MESSAGE_E);
 }
