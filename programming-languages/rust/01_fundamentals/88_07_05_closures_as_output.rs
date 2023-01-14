eaa0f8c4f39452d0b6c61d0051922c23a76a50b0
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2023-01-14 22:38:16 +0100

    Better 88_7_05

--- programming-languages/rust/01_fundamentals/88_07_05_closures_as_output.rs
+++ programming-languages/rust/01_fundamentals/88_07_05_closures_as_output.rs
@@ -4,17 +4,15 @@ Functions as output
 
 
 
-fn my_func(
-    message: &str
-) -> fn() {
+fn my_func() -> fn() {
     return || {
         println!("calling inside");
     };
 }
 
 fn main() {
-    let f1 = my_func("AAA");
-    let f2 = my_func("BBB");
+    let f1 = my_func();
+    let f2 = my_func();
 
     println!("CALL F1");
     f1();
