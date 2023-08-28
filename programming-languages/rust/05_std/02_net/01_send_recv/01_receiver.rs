261f0d8a37831ccf8aa2e01d1a2062dad66e01d5
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2023-08-28 12:33:20 +0200

    rust samples+

--- programming-languages/rust/05_std/02_net/01_send_recv/01_receiver.rs
+++ programming-languages/rust/05_std/02_net/01_send_recv/01_receiver.rs
@@ -1,31 +1,49 @@
 use std::io::prelude::*;
 use std::net::*;
-use std::thread;
-use std::time::Duration;
+
+const DEFAULT_BLOCK_SIZE: usize = 32;
 
 fn handle_stream(mut stream: TcpStream) {
     let peer_address = stream.peer_addr().unwrap();
     let addr = peer_address.to_string();
 
-    println!("[CONNECT]\t {addr}");
+    println!("[{addr}] CONNECTED");
 
-    let mut buffer = [0; 50];
-    let mut first_run = true;
-    let mut recv_size: usize = 0;
+    // prepare a buffer to take in data from the socket
+    let mut buffer = [0; DEFAULT_BLOCK_SIZE];
+
     let mut content = String::new();
 
-    while first_run ^ (recv_size != 0) {
+    loop {
         content.clear();
-        first_run = false;
-        recv_size = stream.read(&mut buffer).unwrap();
-        println!("{buffer:?}");
+
+        /*
+        pass data from the socket to our buffer.
+
+        read potentially blocks until new data
+        is received through the socket.
+        */
+        let recv_size = stream.read(&mut buffer).unwrap();
+
+        /*
+        if nothing is received
+        it means the remote socket closed
+        */
+        if recv_size == 0 {
+            break;
+        }
+
+        // handle the received bytes
+        println!("[{addr}] BUFFER: ");
         for byte in buffer {
             if byte == 0 {
                 break;
             };
             content.push(byte.into());
+            print!("{byte} ");
         }
-        println!("{content:?}\n");
+        print!("\n");
+        println!("[{addr}] {content:?}\n");
     }
 
     println!("[{addr}] GOODBYE!");
@@ -35,9 +53,17 @@ fn main() {
     println!("[RECV] START");
 
     println!("[RECV] bind ...");
+
+    // create a new socket and bind it to a listening address
     let listener = TcpListener::bind("127.0.0.1:56789").unwrap();
 
     println!("[RECV] start listening ...");
+
+    /*
+    block - wait for new connections.
+    incoming() returns when there is a new connection.
+    loop continuously through new connections.
+    */
     for stream in listener.incoming() {
         handle_stream(stream.unwrap());
     }
--- programming-languages/rust/05_std/02_net/01_send_recv/02_sender.rs
+++ programming-languages/rust/05_std/02_net/01_send_recv/02_sender.rs
@@ -7,19 +7,22 @@ fn main() {
     println!("[SNDR] START");
 
     println!("[SNDR] CONNECT");
+
+    // open a connection to the listener
     let mut stream = TcpStream::connect("127.0.0.1:56789").unwrap();
 
     let mut val = 1;
+
+    // send messages in a loop
     loop {
+        // compose the message
         let msg = String::from(format!("hello {val}"));
         println!("[SNDR] sending {msg} ...");
+
+        // put the message in the buffer
         stream.write(msg.as_bytes()).ok();
-        // stream.write(&[]).ok();
-        stream.flush().ok();
 
         thread::sleep(Duration::from_millis(500));
         val += 1;
     }
-
-    println!("[SNDR] FIN");
 }
