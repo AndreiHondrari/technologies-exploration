b04f955d6f17301889985ba28869d7d49efb6408
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2023-10-29 08:22:38 +0100

    add(rust/fundamentals): Add looped kinds of iterations.

---
+++ programming-languages/rust/05_std/05_iter/02_02_kinds_of_iterations_in_loops.rs
@@ -0,0 +1,61 @@
+/*
+Iterators - kinds of iterations
+*/
+
+macro_rules! head_print {
+    ($message:expr) => {
+        println!("\n{}", $message);
+        println!("{}", "-".repeat(50));
+    };
+}
+
+/*
+iteration over references.
+loop yields references
+*/
+fn iterator_on_references(values: [u8; 3]) {
+    // each val is a &u8
+    for val in &values {
+        println!("ref val {}", val);
+    }
+}
+
+/*
+iteration over mutable references.
+loop yields mutable references
+*/
+fn iterator_on_mutable_references(mut values: [u8; 3]) {
+    println!("array before mutation:    {:?}", values);
+
+    // each val is a &mut u8
+    for val in &mut values {
+        *val *= 5;
+    }
+
+    println!("array after mutation:     {:?}", values);
+}
+
+/*
+iteration over values.
+loop yields values
+*/
+fn iterator_on_values(values: [u8; 3]) {
+    // each val is a u8
+    for val in values {
+        println!("val {}", val);
+    }
+}
+
+fn main() {
+    let values: [u8; 3] = [11, 22, 33];
+    println!("origina array:    {:?}", values);
+
+    head_print!("Iterator on references");
+    iterator_on_references(values);
+
+    head_print!("Iterator on mutable references");
+    iterator_on_mutable_references(values);
+
+    head_print!("Iterator on values");
+    iterator_on_values(values);
+}
