78b81f962b05ac9193a29eab076b0a25f3ea15f5
Andrew-George Hondrari <andrei.hondrari@gmail.com>
2023-10-29 08:14:21 +0100

    add(rust/fundamentals): Add kinds of iterations sample.

---
+++ programming-languages/rust/05_std/05_iter/02_kinds_of_iterations.rs
@@ -0,0 +1,63 @@
+/*
+Iterators - kinds of iterations
+*/
+
+use std::slice::{Iter, IterMut};
+
+macro_rules! head_print {
+    ($message:expr) => {
+        println!("\n{}", $message);
+        println!("{}", "-".repeat(50));
+    };
+}
+
+/*
+iteration over references.
+next() gives references
+*/
+fn iterator_on_references(values: [u8; 3]) {
+    let mut values_iterator: Iter<'_, u8> = values.iter();
+    let val: &u8 = values_iterator.next().unwrap();
+    println!("value {}", val);
+}
+
+/*
+iteration over mutable references.
+next() gives mutable references
+*/
+fn iterator_on_mutable_references(mut values: [u8; 3]) {
+    println!("array before mutation:    {:?}", values);
+
+    println!("mutate second value ...");
+    let mut values_iterator: IterMut<'_, u8> = values.iter_mut();
+    values_iterator.next(); // go to middle value
+    let val: &mut u8 = values_iterator.next().unwrap();
+    *val = 99;
+
+    println!("array after mutation:     {:?}", values);
+}
+
+/*
+iteration over values.
+next() gives values
+*/
+fn iterator_on_values(values: [u8; 3]) {
+    // let mut i3 = v1.into_iter();
+    let mut values_iterator = IntoIterator::into_iter(values);
+    let val: u8 = values_iterator.next().unwrap();
+    println!("value {}", val);
+}
+
+fn main() {
+    let values: [u8; 3] = [11, 22, 33];
+    println!("origina array:    {:?}", values);
+
+    head_print!("Iterator on references");
+    iterator_on_references(values);
+
+    head_print!("Iterator on mutable references");
+    iterator_on_mutable_references(values);
+
+    head_print!("Iterator on values");
+    iterator_on_values(values);
+}
